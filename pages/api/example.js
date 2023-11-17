d862a2a76ab742757f567dc02a55a62c5d6a2c7a
manuel perez <manuelperez.0000@gmail.com>
2023-11-17 17:20:40 -0400

    init mongose rest api

---
+++ pages/api/example.js
@@ -0,0 +1,3 @@
+export default function Example(req, res) {
+        res.status(200).json({ name: 'John Doe' })
+}
--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,29 @@ import NavBar from '@/components/navBar'
 import Properties from '@/components/porperties'
 import Carousel from '@/components/carousel'
 import Footer from '@/components/footer'
+import mongoose from 'mongoose'
 export default function Home() {
+
+  const dbConnect = async () => {
+    try {
+      await mongoose.connect('mongodb+srv://manuel:BNWJ4EqX7glnl1cq@cluster0.8qhzw.mongodb.net/caamano');
+      console.log('DB connected');
+    } catch (error) {
+      console.log('DB error', error);
+    }
+  }
+
+  dbConnect()
+
   return (
     <div>
       <div className='ws-button'>
-        <Image className='ws-img-btn' src='/img/logo-ws.svg' width={40} height={40} alt='ws'/>
+        <Image className='ws-img-btn' src='/img/logo-ws.svg' width={40} height={40} alt='ws' />
       </div>
       <NavBar />
       <Carousel />
-      <Properties/>
-      <Footer/>
+      <Properties />
+      <Footer />
     </div>
 
   )
